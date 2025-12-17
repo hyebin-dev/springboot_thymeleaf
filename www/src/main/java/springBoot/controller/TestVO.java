e8f16bd1d91277d0efa9687a444ef49d00699cae
hyebin-dev <229252157+hyebin-dev@users.noreply.github.com>
2025-12-17 16:42:53 +0900

    thymeleaf

---
+++ www/src/main/java/springBoot/controller/TestVO.java
@@ -0,0 +1,13 @@
+package springBoot.controller;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestVO {
+    private String name;
+    private int num;
+}
